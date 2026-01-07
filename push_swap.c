388aae11c25953b0023e7e32ab95fbf650cd4958
megyant <megyant@gmail.com>
2026-01-07 21:07:40 +0000

    add files

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mbotelho <mbotelho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 08:39:53 by mbotelho          #+#    #+#             */
-/*   Updated: 2026/01/07 15:55:01 by mbotelho         ###   ########.fr       */
+/*   Updated: 2026/01/07 21:07:31 by mbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,8 @@ int	main(int ac, char **av)
 	
 	stack_a = NULL;
 	stack_b = NULL;
+	if (ac == 1 || (ac == 2 && !argv[1][0]))
+		return (1);
+	else if (ac == 2)
+		argv = split(av[1], ' ');
 }
